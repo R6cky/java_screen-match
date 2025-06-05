5a97e9c5ba30623f76ca14c0f4aecba49bb0e740
r6cky <cleitonrocha321@hotmail.com>
2025-06-05 14:15:58 -0300

    create a new main class, with the name [ PrincipalComListas ]

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,3 +1,5 @@
+package principal;
+
 import modelos.Filme;
 
 import java.util.ArrayList;
---
+++ src/principal/PrincipalComlistas.java
@@ -0,0 +1,15 @@
+package principal;
+
+import modelos.Filme;
+import modelos.Serie;
+
+public class PrincipalComlistas {
+    public static void main(String[] args) {
+        Filme movie1 = new Filme("Blade o caçador de vampiros", 1999);
+        Filme movie2 = new Filme("matrix", 2003);
+        Filme movie3 = new Filme("Eu sou a lenda", 2022);
+        Serie serie = new Serie("Lost", 2000);
+
+
+    }
+}
